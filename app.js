61e4c6aa2d09ab0e1048b722e992b179a397e7a0
CHECHO2 <mijjn@naver.com>
2025-01-05 01:33:43 +0900

    #4 Login
    #4.2-3

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+  alert("clicked!!!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
