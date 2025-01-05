d12d2a9093b25e8dd30af52984ccbb7bbfe90071
CHECHO2 <mijjn@naver.com>
2025-01-05 17:56:30 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
@@ -14,4 +21,3 @@ function handleLinkClick(event) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
