e40e56c41e1f9a97c10fca051c3059e80c412719
CHECHO2 <mijjn@naver.com>
2025-01-05 22:36:14 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
+  paintGreetings(username);
+}
+
+function paintGreetings(tomato) {
+  greeting.innerText = `Hello ${tomato}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
